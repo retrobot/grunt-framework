1a243dc1ff92c1ed48f19c58b789be55caf1faef
vagrant <vagrant@precise64.(none)>
2013-08-30 17:28:55 +0000

    Add fixes - update

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,8 @@ module.exports = function(grunt) {
     },
     watch: {
       options: {
-        livereload: true,
+        livereload: true, // or set livereload: 1337  - recommended 35729
+        spawn: false
       },
       html: {
         files: ['./**/*.{phtml,xml,html}','./**/*.css','!node_modules/**/*.*'],
@@ -91,13 +92,17 @@ module.exports = function(grunt) {
   });
 
   grunt.file.setBase('./');
-
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-livereload');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-mincss');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-rsync');
+
 
   grunt.registerTask('deploy', ['rsync:dist']);
 //  grunt.registerTask('images', ['imagemin:dist']);
